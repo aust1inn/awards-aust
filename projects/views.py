cb31c1638461a7649300428b8fbe3e296749c788
aust1inn <austinotieno364@gmail.com>
2022-06-13 14:39:52 +0300

    creates registration view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,134 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
+from django.contrib import messages
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('index')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+# @login_required(login_url='/accounts/login/')
+def home(request):
+    projects = Project.get_projects()
+    reviews = Reviews.get_reviews()
+    profile = Profile.get_profile()
+
+    current_user = request.user
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            design = form.cleaned_data['design']
+            usability = form.cleaned_data['usability']
+            content = form.cleaned_data['content']
+            review = form.save(commit=False)
+            review.juror = current_user
+            review.design = design
+            review.usability = usability
+            review.content = content
+            review.save()
+        return redirect('home')
+
+    else:
+        form = ReviewForm()
+
+    return render(request,"home.html",{"projects":projects, "reviews":reviews,"form": form,"profile":profile})
+
+@login_required(login_url='/accounts/login/')
+def profile(request,profile_id):
+
+    profile = Profile.objects.get(pk = profile_id)
+    projects = Project.objects.filter(profile_id=profile).all()
+
+    return render(request,"profile.html",{"profile":profile,"projects":projects})
+
+@login_required(login_url='/accounts/login/')
+def add_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('home')
+
+    else:
+        form = NewProfileForm()
+    return render(request, 'new_profile.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def update_project(request):
+    current_user = request.user
+    profiles = Profile.get_profile()
+    for profile in profiles:
+        if profile.user.id == current_user.id:
+            if request.method == 'POST':
+                form = UploadForm(request.POST,request.FILES)
+                if form.is_valid():
+                    upload = form.save(commit=False)
+                    upload.posted_by = current_user
+                    upload.profile = profile
+                    upload.save()
+                    return redirect('home')
+            else:
+                form = UploadForm()
+            return render(request,'upload.html',{"user":current_user,"form":form})
+
+@login_required(login_url='/accounts/login/')
+def add_review(request,pk):
+    project = get_object_or_404(Project, pk=pk)
+    current_user = request.user
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            design = form.cleaned_data['design']
+            usability = form.cleaned_data['usability']
+            content = form.cleaned_data['content']
+            review = form.save(commit=False)
+            review.project = project
+            review.juror = current_user
+            review.design = design
+            review.usability = usability
+            review.content = content
+            review.save()
+            return redirect('home')
+    else:
+        form = ReviewForm()
+        return render(request,'review.html',{"user":current_user,"form":form})
+
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+
+    if 'title' in request.GET and request.GET["title"]:
+        search_term = request.GET.get("title")
+        searched_project = Project.find_project(search_term)
+        message = search_term
+
+        return render(request,'search.html',{"message":message,
+                                             "searched_project":searched_project})
+    else:
+        message = "You haven't searched for any project"
+        return render(request,'search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def all(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    projects = Project.objects.all().filter(posted_by_id=pk)
+    content = {
+        "profile": profile,
+        'projects': projects,
+    }
+    return render(request, 'profile.html', content)
