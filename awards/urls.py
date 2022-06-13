7c629d4b1962a523104535e73735e35ff154a2fd
aust1inn <austinotieno364@gmail.com>
2022-06-13 14:30:45 +0300

    creates signals.py

--- awards/urls.py
+++ awards/urls.py
@@ -22,6 +22,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('projects.urls')),
+    path('profile/', user_views.profile, name='profile'),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
---
+++ projects/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+
+INTEGER_CHOICES= [tuple([x,x]) for x in range(1,10)]
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']        
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude =['posted_by','profile']
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Reviews
+        design= forms.IntegerField(label="Design Rating")
+        usability = forms.IntegerField(label="Usability Rating")
+        content = forms.IntegerField(label="Content Rating")
+        widget=forms.Select(choices=INTEGER_CHOICES)
+        exclude =['project','juror']
+
